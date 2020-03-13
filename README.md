5dc7ecb9d5fb8e1fa1ac8949cf951fcb9f2d9c98
Tsubasa Sakai <javascripter@tsukkun.net>
2020-03-13 21:23:51 +0900

    add files

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # fontawesome.macro
+
+## Description
+This babel macro enables fontawesome icons to be imported automatically when using the following tagged template syntax
+
+Before:
+
+```javascript
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquare } from '@fortawesome/free-solid-svg-icons'
+
+
+<FontAwesome icon={faSquare} />
+```
+
+After:
+```javascript
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas } from 'fontawesome.macro'
+
+<FontAwesome icon={fas`square`} />
+```
+
+## Usage
+
+https://github.com/kentcdodds/babel-plugin-macros/blob/master/other/docs/user.md
+
+Read the above doc to enable babel macros. If you are using create-react-app, babel macros are enabled by default so you should be able to import this macro without configuring anything.
+
+YOu can use the following tagged template macros:
+
+fal -> light
+fas -> solid
+fab -> brands
+fad -> duotone (pro only)
+
+You can specify free or pro in the babel plugin macro config.
+The below config enables use of pro icons(by default this macro uses free icons).
+
+```javascript
+// babel-plugin-macros.config.js
+module.exports = {
+  // ...
+  'fontawesome.macro': {
+    type: 'pro'
+  }
+}
+```
+
+Note that you need to install svg font packages you want to use in your project by yourself.
+
+https://fontawesome.com/how-to-use/with-the-api/setup/importing-icons
+
+```
+yarn add @fortawesome/free-solid-svg-icons @fortawesome/free-regular-svg-icons	@fortawesome/free-brands-svg-icons
+```
---
+++ index.js
@@ -0,0 +1,140 @@
+const { createMacro } = require('babel-plugin-macros')
+
+const { addNamed } = require('@babel/helper-module-imports')
+
+module.exports = createMacro(fontAwesomeMacro, {
+  configName: 'fontawesome'
+})
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1)
+}
+
+function camelCase(str) {
+  return str
+    .split('-')
+    .map((s, index) => {
+      return (
+        (index === 0 ? s[0].toLowerCase() : s[0].toUpperCase()) +
+        s.slice(1).toLowerCase()
+      )
+    })
+    .join('')
+}
+
+function replaceWithImportedIcon(
+  { references, state, babel, source },
+  { prefix, importFrom }
+) {
+  const { types: t } = babel
+
+  references.forEach(path => {
+    const { parentPath } = path
+
+    if (parentPath.type === 'TaggedTemplateExpression') {
+      const quasi = parentPath.node.quasi
+      if (quasi.type === 'TemplateLiteral') {
+        if (quasi.expressions.length > 0 || quasi.quasis.length > 1) {
+          throw parentPath.buildCodeFrameError(
+            'Dynamic expressions are not supported.'
+          )
+        }
+        if (quasi.quasis[0].type === 'TemplateElement') {
+          const iconName = quasi.quasis[0].value.cooked
+
+          const name = `fa${capitalize(camelCase(iconName))}`
+          const alias = `${prefix}${capitalize(camelCase(iconName))}`
+
+          if (!(name in require(importFrom))) {
+            throw parentPath.buildCodeFrameError(
+              `No icon named ${name} is found in ${JSON.stringify(
+                importFrom
+              )}. Maybe you misspelled it?`
+            )
+          }
+
+          const importName = addNamed(path, name, importFrom, {
+            nameHint: alias
+          })
+
+          parentPath.replaceWith(importName)
+        }
+      }
+    }
+  })
+}
+
+function fontAwesomeMacro({ references, state, babel, source, config }) {
+  const { far = [], fas = [], fal = [], fab = [], fad = [] } = references
+
+  const { type = 'free' } = config
+
+  if (!['free', 'pro'].includes(type)) {
+    throw new Error(
+      "fontawesome.macro config type must be either 'free' or 'pro"
+    )
+  }
+
+  replaceWithImportedIcon(
+    {
+      references: fal,
+      state,
+      babel,
+      source
+    },
+    {
+      prefix: 'fal',
+      importFrom: `@fortawesome/${type}-light-svg-icons`
+    }
+  )
+  replaceWithImportedIcon(
+    {
+      references: fas,
+      state,
+      babel,
+      source
+    },
+    {
+      prefix: 'fas',
+      importFrom: `@fortawesome/${type}-solid-svg-icons`
+    }
+  )
+  replaceWithImportedIcon(
+    {
+      references: far,
+      state,
+      babel,
+      source
+    },
+    {
+      prefix: 'far',
+      importFrom: `@fortawesome/${type}-regular-svg-icons`
+    }
+  )
+  replaceWithImportedIcon(
+    {
+      references: fab,
+      state,
+      babel,
+      source
+    },
+    {
+      prefix: 'fab',
+      importFrom: '@fortawesome/free-brands-svg-icons'
+    }
+  )
+  if (type === 'pro') {
+    replaceWithImportedIcon(
+      {
+        references: fad,
+        state,
+        babel,
+        source
+      },
+      {
+        prefix: 'fad',
+        importFrom: '@fortawesome/pro-duotone-svg-icons'
+      }
+    )
+  }
+}
